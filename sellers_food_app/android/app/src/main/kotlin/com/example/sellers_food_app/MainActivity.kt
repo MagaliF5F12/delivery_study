e821dbd13cd829e7f73e29fd10524613c13c2ecf
ypmarty3@mts.ru <ypmarty3@mts.ru>
2025-05-19 21:53:40 +0300

    test

--- sellers_food_app/android/app/src/main/kotlin/com/example/sellers_food_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sellers_food_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- sellers_food_app/android_/app/src/main/kotlin/com/adnanturgay/sellers_food_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.adnanturgay.sellers_food_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
