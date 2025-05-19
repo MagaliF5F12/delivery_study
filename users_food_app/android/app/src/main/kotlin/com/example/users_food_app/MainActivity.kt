d7cdfad9c2e7615f12fe2ea7c99440fe3bd06784
ypmarty3@mts.ru <ypmarty3@mts.ru>
2025-05-19 21:37:49 +0300

    test

--- users_food_app/android/app/src/main/kotlin/com/example/users_food_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.users_food_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- users_food_app/android/app/src/main/kotlin/com/servanius/incorporated/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.servanius.incorporated
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
